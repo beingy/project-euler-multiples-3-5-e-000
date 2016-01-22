1c5c52cf22d8378196822c64507aa3e3433752fb
beingy <beingy@gmail.com>
2016-01-22 05:12:55 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,9 @@
 # Enter your procedural solution here!
+def collect_multiples(number)
+  (1..number.to_i-1).select { |natnum| (natnum.to_i % 3 == 0) || (natnum.to_i % 5 == 0) }
+end
+
+def sum_multiples(number)
+  collect_multiples(number).reduce(:+)
+end
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @number = limit - 1
+  end
+
+  def number
+    @number
+  end
+
+  def collect_multiples
+    (1..@number).select {|token| (token.to_i % 3 == 0) || (token.to_i % 5 == 0)}
+  end
+
+  def sum_multiples
+    collect_multiples.reduce(:+)
+  end
+end
+
+#limit = Multiples.new(10)
+#puts limit.collect_multiples
+#puts limit.sum_multiples
